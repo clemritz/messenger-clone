062e8c5a5d70d6e82e1a91881291d878ea977cdc
Clément Ritz <clement.ritz@orange.fr>
2023-08-13 16:50:52 +0200

    fixing pusher message

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -3,14 +3,14 @@ import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: "eu",
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     cluster: "eu",
   }
