157ea1fa3cb875d2ddac5ff33491ca649acdc185
Clément Ritz <clement.ritz@orange.fr>
2023-08-16 16:27:00 +0200

    status update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,34 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-import ToasterContext from './context/ToasterContext'
-import AuthContext from './context/AuthContext'
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
-import './globals.css'
+import "./globals.css";
 
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import ActiveStatus from "./components/ActiveStatus";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Messenger Application',
-  description: 'Messenger Application',
-}
+  title: "Messenger Application",
+  description: "Messenger Application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
+          <ActiveStatus />
           {children}
         </AuthContext>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
