37b6f1acd3eeca5463668fab6de368b784897a48
Clément Ritz <clement.ritz@orange.fr>
2023-08-17 23:00:19 +0200

    gray update color status offline

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -21,11 +21,14 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
       </div>
-      {isActive && (
+      {isActive ? (
         <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
+      ) : (
+        <span className="absolute block rounded-full bg-gray-200 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
       )}
     </div>
   );
 };
+
 
 export default Avatar;
